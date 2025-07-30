7d2b91e9f521717e18cb8e5b1645c72fe0de2b40
esraa <esraakhalid1977e@gamil.com>
2025-07-30 20:27:22 +0300

    ddddd

---
+++ backend/models/package.js
@@ -0,0 +1,13 @@
+const Package = require("../models/Package");
+
+const createPackage = async (req, res) => {
+  try {
+    const newPackage = new Package(req.body);
+    const saved = await newPackage.save();
+    res.status(201).json(saved);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { createPackage }
